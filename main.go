67e386c90d35643cb3a7e2a2297c106bcb6dc860
Tatsuemon <i10mann-110@ezweb.ne.jp>
2020-09-04 18:18:50 +0900

    rename infra

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "github.com/Tatsuemon/ddd_go/infrastructure/server"
+import (
+	"fmt"
+
+	"github.com/Tatsuemon/ddd_go/infrastructure"
+)
 
 func main() {
+	fmt.Print("ssss")
 	server.Run(8080)
 }
