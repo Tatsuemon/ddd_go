bd9cf236bdc3539a4d91f37e6e33d99f9d3dc2fd
Tatsuemon <i10mann-110@ezweb.ne.jp>
2020-09-04 18:07:23 +0900

    add usecase

---
+++ usecase/user.go
@@ -0,0 +1,48 @@
+package usecase
+
+import (
+	"ddd_go/domain/model"
+	"ddd_go/domain/repository"
+)
+
+// APIのハンドラ用で使用するメソッドのインターフェース
+type UserUseCase interface {
+	GetUsers() ([]*model.User, error)
+	GetUser(id int) (*model.User, error)
+	CreateUser(user *model.User) (*model.User, error)
+	UpdateUser(user *model.User) (*model.User, error)
+	DeleteUser(id int) error
+}
+
+type userUseCase struct {
+	repository.UserRepository
+}
+
+// コンストラクタ
+func NewUserUseCase(r repository.UserRepository) UserUseCase {
+	return &userUseCase{r}
+}
+
+func (u *userUseCase) GetUsers() ([]*model.User, error) {
+	return u.UserRepository.FindAll()
+}
+
+func (u *userUseCase) GetUser(id int) (*model.User, error) {
+	return u.UserRepository.FindByID(id)
+}
+
+func (u *userUseCase) CreateUser(user *model.User) (*model.User, error) {
+	return u.UserRepository.Store(user)
+}
+
+func (u *userUseCase) UpdateUser(user *model.User) (*model.User, error) {
+	return u.UserRepository.Update(user)
+}
+
+func (u *userUseCase) DeleteUser(id int) error {
+	user, err := u.UserRepository.FindByID(id)
+	if err != nil {
+		return nil, err
+	}
+	return u.UserRepository.Delete(user)
+}
